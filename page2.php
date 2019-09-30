d4115b19ee5ad678e5b315d47c4a96afd51b9ceb
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-09-30 11:20:47 +0200

    debut du td3

--- page2.php
+++ page2.php
@@ -1,6 +1,12 @@
 <?php
     include 'utils.inc.php';
 
+    session_start();
+    if($_SESSION['login']!='ok')
+    {
+        die('Erreur d\'authentification');
+    }
+
     start_page('Page 2');
 
     echo 'Ceci est la page 2';
